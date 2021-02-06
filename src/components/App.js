ee1c7e418514ba8ddaa49fc667a3171d78b5fba5
cdunahee36 <dunahee10@gmail.com>
2021-02-06 11:02:44 -0600

    Put the app into seperate folder and added a welcome message.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Welcome from './welcome';
+
+function App() {
+  return (
+    <div className="container">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+function Welcome() {
+  return (
+    <div>
+      <h5>Welcome to the Roster App sports fan!</h5>
+
+    </div>
+  );
+}
+
+export default Welcome;
